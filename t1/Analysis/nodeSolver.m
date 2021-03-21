4acf5f1cb527576fd2bd1d242be08fac90e96c83
JMGGaspar <joaomgomesgaspar@tecnico.ulisboa.pt>
2021-03-21 13:52:14 +0000

    Node workin

--- t1/Analysis/nodeSolver.m
+++ t1/Analysis/nodeSolver.m
@@ -10,17 +10,17 @@ for i= 1:7
   Data(i) = 1/Data(i);
 endfor
 
-G1 = Data(1);
-G2 = Data(2);
-G3 = Data(3);
-G4 = Data(4);
-G5 = Data(5); 
-G6 = Data(6);
-G7 = Data(7);
-Va = Data(8);
-Id = Data(9);
-Kb = Data(10); 
-Kc = Data(11);
+G1 = Data(1)
+G2 = Data(2)
+G3 = Data(3)
+G4 = Data(4)
+G5 = Data(5)
+G6 = Data(6)
+G7 = Data(7)
+Va = Data(8)
+Id = Data(9)
+Kb = Data(10)
+Kc = Data(11)
 I = 1;
 O = 0;
 
@@ -42,10 +42,10 @@ O = vpa (0.0)
 
 
 
-A = [G1 (-G1-G2-G3) G2 O G5 O O O; O Kb+G2 -G3 O -Kb O O O; O Kb O O -Kb-G5 G5 O O; O O O -G6 O O G6+G7 -G7; O G3 O G4 -G3-G4-G5 G5 G7 -G7;...
+A = [G1 (-G1-G2-G3) G2 O G3 O O O; O Kb+G2 -G2 O -Kb O O O; O Kb O O -Kb-G5 G5 O O; O O O -G6 O O G6+G7 -G7; O G3 O G4 -G3-G4-G5 G5 G7 -G7;...
 I O O -I O O O O; O O O Kc*G6 -I O -Kc*G6 I; O O O I O O O O]
 
-B = [O; O; Id; O; O; Va; O; O]
+B = [O; O; Id; O; Id; Va; O; O]
 
 V = A\B
 end
