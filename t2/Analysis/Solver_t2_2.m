bbb90e007e4259d49496601a46d1f9286af3de43
fvalquaresma <filipefcsvalquaresma@gmail.com>
2021-04-02 16:37:03 +0100

    started solving mesh analysis 2

--- t2/Analysis/Solver_t2_2.m
+++ t2/Analysis/Solver_t2_2.m
@@ -1,27 +1,27 @@
-function Solver(Data, V)
+function tau = Solver(Data, V)
 format long
 
 %Get the results from solvers
-I = meshSolver(Data);
+I_2 = meshSolver_t2_2(Data,V);
 V_2 = nodeSolver_t2_2(Data,V);
 
 %define the names for the variables
 Vi = ['V1'; 'V2'; 'V3'; 'V4'; 'V5'; 'V6'; 'V7'; 'V8'];
 Ii = ['Ia'; 'Ib'; 'Ic'; 'Id'];
-Ij = ['Ib'; 'Id'; 'R1'; 'R2'; 'R3'; 'R4'; 'R5'; 'R6'; 'R7'];
+Ij = ['Ib'; 'C' ; 'R1'; 'R2'; 'R3'; 'R4'; 'R5'; 'R6'; 'R7'];
 Ending = '\\ \hline';
 
 %Get the currents on each branch
 %the order is tha same as Ij
-IBranch(1) = I(2);
-IBranch(2) = I(4);
-IBranch(3) = I(1);
-IBranch(4) = I(2);
-IBranch(5) = I(1)+I(2);
-IBranch(6) = I(1)+I(3);
-IBranch(7) = I(4)-I(2);
-IBranch(8) = I(3);
-IBranch(9) = I(3);
+IBranch(1) = I_2(2);
+IBranch(2) = I_2(4);
+IBranch(3) = I_2(1);
+IBranch(4) = I_2(2);
+IBranch(5) = I_2(1)+I_2(2);
+IBranch(6) = I_2(1)+I_2(3);
+IBranch(7) = I_2(4)-I_2(2);
+IBranch(8) = I_2(3);
+IBranch(9) = I_2(3);
 
 %voltage
 FileV = fopen('Voltages_2.tex','w');
@@ -37,7 +37,7 @@ fclose(FileV);
 FileA = fopen('Currents_2.tex','w');
 for i = 1:4
   Ampere = '%s%s & %f %s \n';
-  fprintf(FileA, Ampere, Ii(i,1), Ii(i,2), I(i), Ending)
+  fprintf(FileA, Ampere, Ii(i,1), Ii(i,2), I_2(i), Ending)
 endfor
 %output on Ampere
 fclose(FileA);
@@ -51,5 +51,8 @@ for i = 1:9
 endfor
 %output on mAmpere
 fclose(FileC);
+
+% fazer output tau 
+% fazer output R_eq (Vx/Ix)
 
 end
---
+++ t2/Analysis/meshSolver_t2_2.m
@@ -0,0 +1,33 @@
+function I = meshSolver(Data,V)
+  
+format long
+
+
+%data  = [R1,R2,R3,R4,R5,R6,R7,Va,Id,Kb,Kc]
+R1 = Data(1);
+R2 = Data(2);
+R3 = Data(3);
+R4 = Data(4);
+R5 = Data(5); 
+R6 = Data(6);
+R7 = Data(7);
+Va = Data(8);
+Id = 0;
+Kb = Data(10); 
+Kd = Data(11);
+I = 1;
+O = 0;
+
+%these are Os instead of zeros on purpose (the same for Is instead of ones)
+%Ic and Id refer to the mesh convention, not necessarily to the exercise figure
+
+A = [R1+R3+R4 R3        R4          O;...
+     Kb*(R3)  Kb*(R3)-I O           O;...
+     R4       O         R4+R6+R7-Kd O;...
+     O        O         O           I];
+     
+B = [Va; O; O; Id];
+
+I = A\B
+
+end
